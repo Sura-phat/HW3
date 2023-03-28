deb18f7f28296f471d6359c37f41150ca9299323
Sura-phat <suraphat.phu@mail.kmutt.ac.th>
2023-03-28 23:31:00 +0700

    first commit

---
+++ ข้อ1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x1, y1, x2, y2, x3, y3;
+
+        // get input from user
+        Console.Write("Enter coordinates of first point on the circle: ");
+        string[] point1 = Console.ReadLine().Split(' ');
+        x1 = double.Parse(point1[0]);
+        y1 = double.Parse(point1[1]);
+
+        Console.Write("Enter coordinates of second point on the circle: ");
+        string[] point2 = Console.ReadLine().Split(' ');
+        x2 = double.Parse(point2[0]);
+        y2 = double.Parse(point2[1]);
+
+        Console.Write("Enter coordinates of third point on the circle: ");
+        string[] point3 = Console.ReadLine().Split(' ');
+        x3 = double.Parse(point3[0]);
+        y3 = double.Parse(point3[1]);
+
+        // calculate center of the circle
+        double x = ((y2 - y1) * (y3 * y3 - y1 * y1 + x3 * x3 - x1 * x1) - (y3 - y1) * (y2 * y2 - y1 * y1 + x2 * x2 - x1 * x1)) / (2 * (y3 - y1) * (x2 - x1) - 2 * (y2 - y1) * (x3 - x1));
+        double y = ((x2 - x1) * (x3 * x3 - x1 * x1 + y3 * y3 - y1 * y1) - (x3 - x1) * (x2 * x2 - x1 * x1 + y2 * y2 - y1 * y1)) / (2 * (x3 - x1) * (y2 - y1) - 2 * (x2 - x1) * (y3 - y1));
+
+        // calculate radius of the circle
+        double radius = Math.Sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1) );
+
+        // display center and radius of the circle
+        Console.WriteLine("Center point of the circle: ({0}, {1})", x, y);
+        Console.WriteLine("Radius of the circle: {0}", radius);
+    }
+}
---
+++ ข้อ2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static bool IsPointInPolygon((double, double) point, List<(double, double)> polygon)
+    {
+        // implementation of ray casting algorithm
+        // returns true if the point is inside the polygon, false otherwise
+        double x = point.Item1;
+        double y = point.Item2;
+        bool inside = false;
+        for (int i = 0, j = polygon.Count - 1; i < polygon.Count; j = i++)
+        {
+            if (((polygon[i].Item2 > y) != (polygon[j].Item2 > y)) &&
+                (x < (polygon[j].Item1 - polygon[i].Item1) * (y - polygon[i].Item2) / (polygon[j].Item2 - polygon[i].Item2) + polygon[i].Item1))
+            {
+                inside = !inside;
+            }
+        }
+        return inside;
+    }
+
+    static void Main(string[] args)
+    {
+        List<(double, double)> polygon = new List<(double, double)>();
+        while (true)
+        {
+            Console.Write("Enter vertex coordinates (x y): ");
+            string[] input = Console.ReadLine().Split();
+            double x = double.Parse(input[0]);
+            double y = double.Parse(input[1]);
+            if (x == 0 && y == 0)
+            {
+                break;
+            }
+            polygon.Add((x, y));
+        }
+
+        if (polygon.Count < 3)
+        {
+            Console.WriteLine("Error: Polygon must have at least 3 vertices.");
+        }
+        else
+        {
+            Console.WriteLine("Polygon vertices:");
+            foreach ((double, double) vertex in polygon)
+            {
+                Console.WriteLine(vertex);
+            }
+
+            while (true)
+            {
+                Console.Write("Enter point coordinates (x y): ");
+                string[] input = Console.ReadLine().Split();
+                double x = double.Parse(input[0]);
+                double y = double.Parse(input[1]);
+                if (x != 0 || y != 0)
+                {
+                    if (IsPointInPolygon((x, y), polygon))
+                    {
+                        Console.WriteLine("The point is inside the polygon.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The point is outside the polygon.");
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
